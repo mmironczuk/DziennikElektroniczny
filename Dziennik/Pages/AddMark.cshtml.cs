5d2a43d706335ce4e66a31001cd8f8d2a5dfd377
mmironczuk <74499627+mmironczuk@users.noreply.github.com>
2020-11-16 20:51:16 +0100

    commit_test

--- Dziennik/Pages/AddMark.cshtml.cs
+++ Dziennik/Pages/AddMark.cshtml.cs
@@ -17,9 +17,35 @@ namespace Dziennik.Pages
         {
             _configuration = configuration;
         }
-        public void OnGet(int _iducznia)
+        public void OnGet(int _iducznia, int _idprzedmiotu, int _idnauczyciela)
         {
-
+            ocena.iducznia = _iducznia;
+            ocena.idprzedmiotu = _idprzedmiotu;
+            ocena.idnauczyciela = _idnauczyciela;
+        }
+        public IActionResult OnPost(Ocena ocena)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            string DziennikDBcs = _configuration.GetConnectionString("DziennikDB");
+            SqlConnection con = new SqlConnection(DziennikDBcs);
+            string sql = "INSERT INTO Ocena (idnauczyciela, idprzedmiotu, iducznia, wartosc, opis) VALUES (@IDNAUCZYCIELA, @IDPRZEDMIOTU, @IDUCZNIA, @WARTOSC, @OPIS)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@IDNAUCZYCIELA", ocena.idnauczyciela);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@IDUCZNIA", ocena.iducznia);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@IDPRZEDMIOTU", ocena.idprzedmiotu);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@WARTOSC", ocena.mark);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@OPIS", ocena.opis);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return RedirectToPage("Index");
         }
     }
 }
