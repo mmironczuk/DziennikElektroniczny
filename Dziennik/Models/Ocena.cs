2251ab08f970dec37a73f7d57a0ec45135a0b56f
mmironczuk <74499627+mmironczuk@users.noreply.github.com>
2020-11-16 20:20:35 +0100

    Mark Class

---
+++ Dziennik/Models/Ocena.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dziennik.Models
+{
+    public class Ocena
+    {
+        public int iducznia { get; set; }
+        public decimal mark { get; set; }
+        public string opis { get; set; }
+    }
+}
