4d6664f8bbdebe4fb2ac5d122d5edd8982727f15
Tomke <zenonqrde@wp.pl>
2020-12-02 19:55:51 +0100

    Dodanie edycji wydarzeń i przedmioty

---
+++ Dziennik/Pages/EdycjaWydarzenia.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dziennik.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dziennik.Pages
+{
+    public class EdycjaWydarzeniaModel : PageModel
+    {
+        public Wydarzenie wydarzenie;
+        public static Wydarzenie wydarzenieToShow;
+        public static List<Przedmiot> przedmioty;
+        public static List<Klasa> klasy;
+
+        public void OnGet(int idWydarzenia)
+        {
+            klasy = new List<Klasa>();
+            przedmioty = new List<Przedmiot>();
+            wydarzenie = new Wydarzenie();
+            wydarzenieToShow = new Wydarzenie();
+
+            wydarzenieToShow = wydarzenie;
+
+            wydarzenieToShow.nazwa = "abc";
+            wydarzenieToShow.opis = "opis";
+            wydarzenieToShow.data = DateTime.Now;
+        }
+    }
+}
