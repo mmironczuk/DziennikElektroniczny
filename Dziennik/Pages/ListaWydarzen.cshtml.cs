bb3c9d587f6fecd84ec18fa967f2dd7b27bdeda7
Voltinus <voltinusmail@gmail.com>
2020-12-03 20:40:38 +0100

    Wyświetlanie wydarzeń

--- Dziennik/Pages/ListaWydarzen.cshtml.cs
+++ Dziennik/Pages/ListaWydarzen.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Dziennik.DAL;
 using Dziennik.Models;
@@ -12,16 +13,42 @@ namespace Dziennik.Pages
 {
     public class ListaWydarzenModel : PageModel
     {
-        public static ObservableCollection<Wydarzenie> wydarzenia;
-
-        public static int typ_uzytkownika;
-
+        [BindProperty]
+        public ObservableCollection<Wydarzenie> wydarzenia { get; set; }
+        [BindProperty]
+        public int typ_uzytkownika { get; set; }
         public MainDatabase db = new MainDatabase();
 
         public void OnGet()
         {
-            wydarzenia = db.GetWydarzeniaAll();
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.Name);
+            var login = claim.Value;
 
+            typ_uzytkownika = db.GetKontoLogin(login).typ_uzytkownika;
+            wydarzenia = db.GetWydarzeniaAll();
+            if (typ_uzytkownika == 2) {
+                Uczen uczen = db.GetUczenKonto(db.GetKontoLogin(login).Id_konta);
+
+                foreach (Wydarzenie w in wydarzenia)
+                {
+                    if (w.Klasa.Id_klasy != uczen.Klasa.Id_klasy)
+                    {
+                        wydarzenia.Remove(w);
+                    }
+                }
+            } else if (typ_uzytkownika == 1)
+            {
+                Nauczyciel nauczyciel = db.GetNauczycielKonto(db.GetKontoLogin(login).Id_konta);
+
+                foreach (Wydarzenie w in wydarzenia)
+                {
+                    if (w.Nauczyciel.Id_nauczyciela != nauczyciel.Id_nauczyciela)
+                    {
+                        wydarzenia.Remove(w);
+                    }
+                }
+            }
 
             /*for (int i = 0; i < 6; i++)
             {
