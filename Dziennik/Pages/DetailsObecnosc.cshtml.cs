86b40e053dc726366fd6c4b4185190f8eec6d9cb
mmironczuk <74499627+mmironczuk@users.noreply.github.com>
2020-12-04 19:02:01 +0100

    wyswietlanie obecnosci

---
+++ Dziennik/Pages/DetailsObecnosc.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dziennik.DAL;
+using Dziennik.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dziennik.Pages
+{
+    public class DetailsObecnoscModel : PageModel
+    {
+        public MainDatabase mainDatabase = new MainDatabase();
+        public Obecnosc obecnosc;
+        public void OnGet(int id)
+        {
+            obecnosc = mainDatabase.GetObecnosc(id);
+        }
+    }
+}
