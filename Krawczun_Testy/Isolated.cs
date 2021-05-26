ea70ac0029aac5ae74025a6a59660907827699d0
KrawczunDamian <73105018+KrawczunDamian@users.noreply.github.com>
2021-05-26 22:19:24 +0200

    #DZELCB2020-111 #time 7h Próba wprowadzenia testu integracyjnego
    Rozróżnienie testów integracyjnych od jednostkowych. Próba znalezienia możliwości testów jednostkowych zakończona niepowodzeniem. Próba wprowadzenia testów integracyjnych z problemami (łączenie się z bazą danych w teście i uruchomienie testu).

---
+++ Krawczun_Testy/Isolated.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Krawczun_Testy
+{
+    class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+        public ActionTargets Targets 
+        {
+            get { return ActionTargets.Test; }
+        }
+
+        public void BeforeTest(ITest test)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(ITest test)
+        {
+            _transactionScope.Dispose();
+        }
+
+    }
+}
