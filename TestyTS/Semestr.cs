c01b791365a212ef966c00e719247c708a697eca
Tomke <zenonqrde@wp.pl>
2021-06-10 03:27:43 +0200

    #DZELCB2020-110 #time 2h Testy
    Wykonanie testów dla filtrowania oraz dzielenia na semestry.

---
+++ TestyTS/Semestr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestyTS
+{
+    public class Semestr
+    {
+        private int idSemestru;
+        public DateTime dataRozpoczecia;
+        public DateTime dataZakonczenia;
+
+        public Semestr(int id, DateTime dr, DateTime dz)
+        {
+            idSemestru = id;
+            dataRozpoczecia = dr;
+            dataZakonczenia = dz;
+        }
+    }
+}
